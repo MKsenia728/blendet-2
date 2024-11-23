1ce114de10a2edfd9c73aecc32c986bbd6147f35
Mihail <mykh.shev4enko@gmail.com>
2024-11-23 13:29:55 +0200

    done

--- js/task-12.js
+++ js/task-12.js
@@ -1,2 +1,13 @@
 // Напишіть функцію, що прибирає значення, які повторюються
 // Очікується["a",1, 2, "b",3, 1, 2,"a"] => ["a", 1, 2 ,"b" , 3]
+function removeDuplicate(arr) {
+  const newArr = [];
+  for (const elem of arr) {
+    if (!newArr.includes(elem)) {
+      newArr.push(elem);
+    }
+  }
+  return newArr;
+}
+
+console.log(removeDuplicate(["a", 1, 2, "b", 3, 1, 2, "a"]));
