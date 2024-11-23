dd75f66a09a63da7d5b433a8f91ea26e664189a7
Oksana Molchanova <mksenia728@gmail.com>
2024-11-23 12:11:13 +0100

    task 11

--- js/task-11.js
+++ js/task-11.js
@@ -1,2 +1,26 @@
 // Напишіть функцію, яка перетворює глибокий масив в одномірний. Не використовувати array.flat()
-// Очікується[1, 2, [3, 4, [5]]] => [1, 2, 3, 4, 5]
+// Очікується[1, 2, [3, 4, [5, [6, 7, [8]]]], [9,10]] => [1, 2, 3, 4, 5]
+let count = 0;
+
+function changeArr(arr) {
+  const newArr =[];
+  for (const elem of arr) {
+    if (Array.isArray(elem)) {
+      // for (const itemElem of changeArr(elem)) {
+      
+        newArr.push(...changeArr(elem));
+        // console.log(newArr);
+      // }
+  
+    } else {
+      newArr.push(elem);
+    }
+  }
+  return newArr;
+}
+
+console.log([1, 2, [3, 4, [5, [6, 7, [8]]]], [9,10]].flat(42));
+const arr = [1, 2, [3, 4, [5, [6, 7, [8]]]], [9,10]].flat(42);
+
+
+console.log(changeArr([1, 2, [3, 4, [5, [6, 7, [8], [9,10]]]]]));
